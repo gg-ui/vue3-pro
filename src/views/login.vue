68eb93b00af872c5f7cde7ea40d2362ee211b790
liuzenghui0315 <1679071078@qq.com>
2026-02-06 14:12:55 +0800

    登录样式

---
+++ src/views/login.vue
@@ -0,0 +1,104 @@
+<template>
+  <el-form
+  class="loginForm"
+    :model="sizeForm"
+    label-width="auto"
+    :label-position="labelPosition"
+    :size="size"
+  >
+  <h1 class="title">TEST系统</h1>
+    <el-form-item label="name">
+      <el-input v-model="sizeForm.name" />
+    </el-form-item>
+    <el-form-item label="password">
+      <el-select
+        v-model="sizeForm.password"
+        placeholder="please select your zone"
+      >
+        <el-option label="Zone one" value="shanghai" />
+        <el-option label="Zone two" value="beijing" />
+      </el-select>
+    </el-form-item>
+    <el-form-item>
+      <el-button :class="active?'active':''" class="loginBtn" @click="onSubmit">login</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+
+import type { ComponentSize, FormProps } from 'element-plus'
+
+const size = ref<ComponentSize>('default')
+const labelPosition = ref<FormProps['labelPosition']>('right')
+
+const sizeForm = reactive({
+  name: '',
+  password:'',
+})
+const active=ref(false)
+
+function onSubmit() {
+  active.value = true
+ console.log(6666);
+
+}
+</script>
+
+<style lang="scss" scoped>
+@keyframes spin {
+  0% {
+    --rotate: 0deg;
+  }
+  100% {
+    --rotate: 360deg;
+  }
+}
+@property --rotate {
+  syntax: "<angle>";
+  initial-value: 132deg;
+  inherits: false;
+}
+.loginForm{
+  position: absolute;
+  user-select: none;
+  top:50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 600px;
+  height: 300px;
+  background-color: #fff;
+  z-index: 50;
+  padding: 40px 20px;
+  .title{
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .loginBtn{
+    width: 87%;
+    margin-left: 13%;
+    font-size: 16px;
+    background-image: linear-gradient(to right, red, aqua, #00008B);
+
+  }
+  .active{
+    background-image: linear-gradient(to right, green, #1804ed, yellow);
+
+  }
+  &::before{
+     content: "";
+     width: 104%;
+     height: 102%;
+     border-radius: 8px;
+     background-image: linear-gradient(
+       var(--rotate)
+       , #5ddcff, #3c67e3 43%, #4e00c2);
+     position: absolute;
+     z-index: -1;
+     top: -1%;
+     left: -2%;
+     animation: spin 2.5s linear infinite;
+  }
+}
+</style>
