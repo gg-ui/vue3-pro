1cf8cbca71b45d6c3c3f6c1f04ed7f54f0bc3854
g <1481213218@qq.com>
2026-02-10 10:10:35 +0800

    路由的动态导入

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from 'vue-router';
 
-const routeContext = import.meta.globEager('./modules/*/*.ts');
-const route = Object.keys(routeContext).map((key) => routeContext[key].default)
+// glob添加eager：true，vite 在构建时立即加载所有匹配文件，并将它们作为已解析的模块包含在结果中
+// 即可以直接调用.default，若不添加eager：true，routeContext结果是一个promise的function函数
+const routeContext = import.meta.glob('./modules/*/*.ts', { eager: true });
+const route = Object.keys(routeContext).map((key) => {
+  const defaults = routeContext[key] as any
+  return defaults.default
+})
 const routes: RouteRecordRaw[] = [].concat(...route);
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
-
 export default router;
